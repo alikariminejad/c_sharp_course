0a80604f5fab11706fc7be4d7543b79cea426c5d
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-02-27 23:43:44 +0330

    working with console read and write line

---
+++ 2_My_first_app/2_My_first_app/Program.cs
@@ -0,0 +1,32 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        //System.Console.WriteLine("give me a number from 1 to 10:");
+        //string x = System.Console.ReadLine();
+        //System.Console.Clear();
+        //System.Console.WriteLine(x);
+        //System.Console.ReadKey();
+
+        // Print messages line by line
+        System.Console.WriteLine("Welcome");
+        System.Console.WriteLine("To");
+        System.Console.WriteLine("C# Programming!!!");
+
+        // Print messages in one line
+        System.Console.Write("Welcome");
+        System.Console.Write(" to ");
+        System.Console.Write("c# Programmin!!!");
+        // waiting to press a key
+        System.Console.ReadKey();
+
+        // Clear the screen
+        System.Console.Clear();
+
+
+        System.Console.WriteLine("Thank you my friend!");
+        System.Console.ReadKey();
+
+
+    }
+}
