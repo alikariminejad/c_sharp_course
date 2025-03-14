60cd48be2db296b31e48e18998930cd4d8dd44f0
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-03-14 15:53:56 +0330

    variables examples

---
+++ 3_VariablesExample/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Sample
+{
+    static void Main()
+    {
+        // declare the variable
+        string StudentName = "Ali";
+        int Age = 26;
+
+        // print value
+        System.Console.Write("Hey ");
+        System.Console.Write(StudentName);
+        System.Console.Write(", your age is: ");
+        System.Console.Write(Age);
+
+        // wait for pressing some key on keyboard
+        System.Console.ReadKey();
+    }
+}
