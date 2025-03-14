edaf260a9ba233b64386d26dd9602a64dea9fc4a
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-03-14 16:32:32 +0330

    primitive types example

---
+++ 4_PrimitiveTypesExample/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        sbyte a = sbyte.MaxValue;
+        sbyte b = 100;
+        sbyte c = default(sbyte);
+
+        byte d = byte.MaxValue;
+        short e = short.MaxValue;
+        ushort f = ushort.MaxValue;
+        int g = int.MaxValue;
+        uint h = uint.MaxValue;
+        long i = long.MaxValue;
+        ulong j = ulong.MinValue;
+        float k = float.MinValue;
+        double l = double.MaxValue;
+        decimal m = decimal.MaxValue;
+        char n = 'G';
+        string o = "lkjasldfkjslf";
+        bool p = true;
+
+        System.Console.WriteLine(a);
+        System.Console.WriteLine(b);
+        System.Console.WriteLine(c);
+        System.Console.WriteLine(d);
+        System.Console.WriteLine(e);
+        System.Console.WriteLine(f);
+        System.Console.WriteLine(g);
+        System.Console.WriteLine(h);
+        System.Console.WriteLine(i);
+        System.Console.WriteLine(j);
+        System.Console.WriteLine(k);
+        System.Console.WriteLine(l);
+        System.Console.WriteLine(m);
+        System.Console.WriteLine(n);
+        System.Console.WriteLine(o);
+        System.Console.WriteLine(p);
+
+        System.Console.ReadKey();
+    }
+}
